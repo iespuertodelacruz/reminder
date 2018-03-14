ad9927884daac584d8be3d026d26e448f5cb75f7
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-03-14 20:34:56 +0100

    Add left and top options

--- reminder.py
+++ reminder.py
@@ -2,12 +2,14 @@
 Reminder lets you change the badge on the header of website.
 
 Usage:
-	reminder.py set <url> [--target=<url>] [--size=<px>]
+	reminder.py set <url> [--target=<url>] [--size=<px>] [--left=<px>] [--top=<px>]
 	reminder.py remove
 
 Options:
     --target=<url>    Target URL of the badget.
     --size=<px>       Height size of the badget [default: 300].
+    --left=<px>       Relative left position of the badget [default: 150].
+    --top=<px>        Relative top position of the badget [default: 0].
 """
 from docopt import docopt
 import config
@@ -16,7 +18,7 @@ import os
 import datetime
 
 
-def set_badge(url, target, size):
+def set_badge(url, target, size, left, top):
 	filename = "badge_" + datetime.datetime.now().strftime("%s") + ".png"
 	path = os.path.join(config.BADGES_PATH, filename)
 	os.system("curl --silent -o {} {}".format(path, url))
@@ -27,6 +29,8 @@ def set_badge(url, target, size):
 	else:
 		target = target.replace("/", "\/")
 	os.system("""sed -i -E 's/id="badge_target" href="\S*">/id="badge_target" href="{}">/' {}""".format(target, config.HEADER_PATH))
+	os.system('sed -i -E "s/left: -?[0-9]+px; \/\*badge_left/left: {}px; \/\*badge_left/" {}'.format(left, config.HEADER_PATH))
+	os.system('sed -i -E "s/top: -?[0-9]+px; \/\*badge_top/top: {}px; \/\*badge_top/" {}'.format(top, config.HEADER_PATH))
 
 
 def clean_badges():
@@ -37,7 +41,7 @@ def clean_badges():
 
 def remove_badges():
 	clean_badges()
-	set_badge(config.TRANSPARENT_BADGE_URL, None, 1)
+	set_badge(config.TRANSPARENT_BADGE_URL, None, 1, 0, 0)
 
 
 if __name__ == "__main__":
@@ -46,7 +50,9 @@ if __name__ == "__main__":
 		set_badge(
 			arguments["<url>"],
 			arguments["--target"],
-			arguments["--size"]
+			arguments["--size"],
+			arguments["--left"],
+			arguments["--top"]
 		)
 	elif arguments["remove"]:
 		remove_badges()
