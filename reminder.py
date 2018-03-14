1b1c13518ac895a20d281c44849e8a026a1e458b
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-03-14 20:03:50 +0100

    Add size option

--- reminder.py
+++ reminder.py
@@ -2,11 +2,12 @@
 Reminder lets you change the badge on the header of website.
 
 Usage:
-	reminder.py set <url> [--target=<url>]
+	reminder.py set <url> [--target=<url>] [--size=<px>]
 	reminder.py remove
 
 Options:
     --target=<url>    Target URL of the badget.
+    --size=<px>       Height size of the badget [default: 300].
 """
 from docopt import docopt
 import config
@@ -15,11 +16,11 @@ import os
 import datetime
 
 
-def set_badge(url, target=None):
+def set_badge(url, target, size):
 	filename = "badge_" + datetime.datetime.now().strftime("%s") + ".png"
 	path = os.path.join(config.BADGES_PATH, filename)
 	os.system("curl --silent -o {} {}".format(path, url))
-	os.system("mogrify -resize x300 {}".format(path))
+	os.system("mogrify -resize x{} {}".format(size, path))
 	os.system('sed -i -E "s/badge_[0-9]+\.png/{}/" {}'.format(filename, config.HEADER_PATH))
 	if target is None:
 		target = ""
@@ -36,7 +37,7 @@ def clean_badges():
 
 def remove_badges():
 	clean_badges()
-	set_badge(config.TRANSPARENT_BADGE_URL)
+	set_badge(config.TRANSPARENT_BADGE_URL, None, 1)
 
 
 if __name__ == "__main__":
@@ -44,7 +45,8 @@ if __name__ == "__main__":
 	if arguments["set"]:
 		set_badge(
 			arguments["<url>"],
-			arguments["--target"]
+			arguments["--target"],
+			arguments["--size"]
 		)
 	elif arguments["remove"]:
 		remove_badges()
