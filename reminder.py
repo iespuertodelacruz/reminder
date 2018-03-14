2d0ce90407f239b62ff8ffe36ea374a3d0502c99
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-03-14 19:24:07 +0100

    Add target as option to badge

--- reminder.py
+++ reminder.py
@@ -2,7 +2,7 @@
 Reminder lets you change the badge on the header of website.
 
 Usage:
-	reminder.py set <url> 
+	reminder.py set <url> [<target>]
 	reminder.py remove
 """
 from docopt import docopt
@@ -12,12 +12,16 @@ import os
 import datetime
 
 
-def set_badge(url):
+def set_badge(url, target):
 	filename = "badge_" + datetime.datetime.now().strftime("%s") + ".png"
 	path = os.path.join(config.BADGES_PATH, filename)
 	os.system("curl --silent -o {} {}".format(path, url))
 	os.system("mogrify -resize x300 {}".format(path))
 	os.system('sed -i -E "s/badge_[0-9]+\.png/{}/" {}'.format(filename, config.HEADER_PATH))
+	if not target:
+		target = ""
+	target = target.replace("/", "\/")
+	os.system("""sed -i -E 's/id="badge_target" href="\S*">/id="badge_target" href="{}">/' {}""".format(target, config.HEADER_PATH))
 
 
 def clean_badges():
@@ -32,8 +36,11 @@ def remove_badges():
 
 
 if __name__ == "__main__":
-    arguments = docopt(__doc__)
-    if arguments["set"]:
-        set_badge(arguments["<url>"])
-    elif arguments["remove"]:
-        remove_badges()
+	arguments = docopt(__doc__)
+	if arguments["set"]:
+		set_badge(
+			arguments.get("<url>"),
+			arguments.get("<target>")
+		)
+	elif arguments["remove"]:
+		remove_badges()
